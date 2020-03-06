552382123326a36ada24c91c986c49de3b3f5aab
matao <tt_matao@163.com>
2020-03-06 21:00:22 +0800

    新增了  测试碎片

--- app/src/main/java/com/jy/firsttest/shop/ui/fragment/TestFragment.java
+++ app/src/main/java/com/jy/firsttest/shop/ui/fragment/TestFragment.java
@@ -1,8 +1,12 @@
 package com.jy.firsttest.shop.ui.fragment;
 
+import android.util.Log;
+
 /**
  * 作者：matao
  */
 public class TestFragment {
-
+    public void show(){
+        Log.d("","");
+    }
 }
