0cb5bc41dff2f60a67d9284aa4a9164866bc5a58
matao <tt_matao@163.com>
2020-03-06 21:19:48 +0800

    cart加注释了

--- app/src/main/java/com/jy/firsttest/shop/ui/fragment/CartFragment.java
+++ app/src/main/java/com/jy/firsttest/shop/ui/fragment/CartFragment.java
@@ -11,7 +11,9 @@ import androidx.fragment.app.Fragment;
 
 import com.jy.firsttest.shop.R;
 
-
+/**
+ * 加注释
+ */
 public class CartFragment extends Fragment {
 
     public static CartFragment newInstance(){
